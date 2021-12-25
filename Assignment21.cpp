a6fdb97da1c470cd5cd06513de068408abf02354
Alya <55447254+alya-atm@users.noreply.github.com>
2021-12-25 17:47:41 +0300

    Add files via upload

---
+++ Assignment21.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "mpi.h"
+
+
+int main(int argc, char **argv)
+{
+	int rank;
+	MPI_Init(&argc, &argv);
+	MPI_File fh;
+	char buf[10];
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0) {
+    	FILE *fp = fopen("file1.txt", "w");
+		if (fp != NULL)
+	{
+	fputs("proident est esse Lorem Lorem exercitation ullamco aliquip nostrud officia laborum ex enim reprehenderit laboris voluptate laboris proident pariatur nostrud dolore velit et dolore et", fp);
+	fclose(fp);
+}
+} 
+	MPI_File_open(MPI_COMM_WORLD, "file1.txt", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+	MPI_File_set_view(fh, rank * 10, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+	MPI_File_read_all(fh, buf, 10, MPI_CHAR, MPI_STATUS_IGNORE);
+	printf("process %d, buf=%s\n", rank, buf);
+	MPI_File_close(&fh);
+	MPI_Finalize();
+}
